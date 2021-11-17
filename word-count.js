751894950699eea78ae1523cd6fda75c7cb0167a
bburba1 <blake.burbach@ampf.com>
2021-11-17 16:55:31 -0600

    reworded some explanations

--- word-count.js
+++ word-count.js
@@ -3,23 +3,27 @@
 // convenience to get you started writing code faster.
 //
 
-/* RegEx to see if the apostrophe is present and between word characters
-  `special` OR `789` OR `that's` will work
+/*
+  RegEx to see if the apostrophe is present and between word characters
+  `special` OR `789` OR `that's` will be matched
 
-  `'123` or `password'` or any punctuation, whitespace, or vertical whitespace WILL NOT work
+  `'123` or `password'` will ignore the apostrophes and just return the word without them
+
+  will ignore anything else out that doesn't match
 */
 const REGULAR_WORDS_AND_CONTRACTIONS = /\w+('\w+)?/g;
 
 
 export const countWords = (sentence) => {
-  // match will create an array of strings that match the regex
+  // match will create an array of all words that match the regex
   const scrubbedAndSplitWords = sentence.toLowerCase().match(REGULAR_WORDS_AND_CONTRACTIONS);
   const wordCountObject = {};
 
   // for each unique word we find, we'll create a key in the wordCountObject
-  // if we find that the key already exists, we add to the existing count
+  // if the key already exists, we add to the existing count of the key
   scrubbedAndSplitWords.forEach(word =>
     wordCountObject[word] = (wordCountObject[word] || 0) + 1
   );
+
   return wordCountObject;
 };
