36c8e5b947d38d60d7afd565e96e6d274ddfeef1
bburba1 <blake.burbach@ampf.com>
2021-11-17 16:51:25 -0600

    refactored regex, added match string method to handle the heavy lifting instead of all of the replaces I was using, and now passing all test cases

--- word-count.js
+++ word-count.js
@@ -3,27 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-// RegEx to find all of the following characters within the string
-const SPECIAL_CHARACTERS = /[.,\/#!$%\^&\*;:{}=\-_`~()"]/g;
-// RegEx to see if the apostrophe is between word characters - i.e. `that's` NOT `'123` or `password'`
-const IS_CONTRACTION = /(\w'\w)/g;
+/* RegEx to see if the apostrophe is present and between word characters
+  `special` OR `789` OR `that's` will work
 
-const removeApostropheFromWordsIfNotAContraction = (words) => words.map(word => {
-  let wordWithApostropheRemoved = null;
-  if(word.includes(`'`) && !IS_CONTRACTION.test(word)) {
-    wordWithApostropheRemoved = word.replace(`'`, "");
-  }
-  return wordWithApostropheRemoved ? wordWithApostropheRemoved : word;
-});
+  `'123` or `password'` or any punctuation, whitespace, or vertical whitespace WILL NOT work
+*/
+const REGULAR_WORDS_AND_CONTRACTIONS = /\w+('\w+)?/g;
+
 
 export const countWords = (sentence) => {
-  const lowerCaseSentence = sentence.toLowerCase();
-  const specialCharactersRemoved = lowerCaseSentence.replace(SPECIAL_CHARACTERS, " ");
-  const seperatedWordArray = specialCharactersRemoved.split(" ");
-  const apostrophesRemovedFromNonContractions = removeApostropheFromWordsIfNotAContraction(seperatedWordArray);
+  // match will create an array of strings that match the regex
+  const scrubbedAndSplitWords = sentence.toLowerCase().match(REGULAR_WORDS_AND_CONTRACTIONS);
   const wordCountObject = {};
 
-  apostrophesRemovedFromNonContractions.forEach(word =>
+  // for each unique word we find, we'll create a key in the wordCountObject
+  // if we find that the key already exists, we add to the existing count
+  scrubbedAndSplitWords.forEach(word =>
     wordCountObject[word] = (wordCountObject[word] || 0) + 1
   );
   return wordCountObject;
